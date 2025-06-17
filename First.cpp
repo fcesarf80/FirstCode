1945e1c6694a5cdc5fd93131b758aa837f154c5c
F Cesar Farias <fcesarf80@gmail.com>
2025-06-17 10:38:29 +0100

    Add project files.

---
+++ First.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<stdlib.h>
+
+int main()
+{
+    std::cout << std::endl;
+    std::cout << "Vou " << std::endl;
+    std::cout << "aprender " << std::endl;
+    std::cout << "a " << std::endl;
+    std::cout << "programar " << std::endl;
+    std::cout << "na " << std::endl;
+    std::cout << "linguagem " << ::std::endl;
+    std::cout << "C++." << ::std::endl;
+    std::cout << std::endl;
+
+    system("pause");
+    return 0;
+}
